50b7b5f829ed5dc3775ea18442ef7e41ea25a968
agate <agate.hao@gmail.com>
2013-09-14 12:02:14 +0800

    added rails init project dir

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Busykoala::Application.config.secret_key_base = 'df62103df9658309e735c6e9235eacaeb86f00f2aef5a81b723e296dbc6621eb253f49777057ff203df7b05ff65248c14f0d5d5948f39da0bc1a33b22705b034'
---
+++ server/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Busykoala::Application.config.session_store :cookie_store, key: '_busykoala_session'
